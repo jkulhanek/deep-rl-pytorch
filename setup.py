1ae4a11d04ad7848a36c89b6216c48930b1bc152
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-03-05 00:31:07 +0000

    Add submodules

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='deep_rl',
     version='0.1.0',
-    packages=['deep_rl',],
+    packages=['deep_rl', 'deep_rl.a2c', 'deep_rl.common', 'deep_rl.deepq'],
     author='Jonáš Kulhánek',
     author_email='jonas.kulhanek@live.com',
     license='MIT License',
