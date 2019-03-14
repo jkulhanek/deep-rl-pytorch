814ec0f173dd36f00e087f2e763fdee8a2919d51
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-03-14 12:22:59 +0000

    Fix deepmindlab environment

--- environments/gym_deepmindlab/__init__.py
+++ environments/gym_deepmindlab/__init__.py
@@ -1,5 +1,5 @@
 LEVELS = ['lt_chasm', 'lt_hallway_slope', 'lt_horseshoe_color', 'lt_space_bounce_hard', \
-'nav_maze_random_goal_01','nav_maze_random_goal_02', 'nav_maze_random_goal_03', 'nav_maze_static_01' \
+'nav_maze_random_goal_01','nav_maze_random_goal_02', 'nav_maze_random_goal_03', 'nav_maze_static_01', \
 'nav_maze_static_02', 'seekavoid_arena_01', 'stairway_to_melon']
 
 def _to_pascal(text):
