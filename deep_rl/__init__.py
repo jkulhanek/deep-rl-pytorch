a88eabdcc50fd04126d961a45a6875e1c71bac54
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-03-24 14:49:53 +0000

    Add multistep schedule

--- deep_rl/__init__.py
+++ deep_rl/__init__.py
@@ -1,7 +1,8 @@
 from .registry import make_agent, make_trainer, register_agent, register_trainer
 from .configuration import configuration
+from .common import schedules
 
-VERSION = '0.2.1'
+VERSION = '0.2.2'
 
 def configure(**kwargs):
     configuration.update(kwargs)
--- deep_rl/common/schedules.py
+++ deep_rl/common/schedules.py
@@ -1,5 +1,20 @@
 from ..core import Schedule
 
+class MultistepSchedule(Schedule):
+    def __init__(self, initial_value, steps, total_iteractions):
+        self.initial_value = initial_value
+        self.steps = steps
+        self.total_iteractions = total_iteractions
+        self.time = None
+
+    def __call__(self):
+        val = self.initial_value
+        for i, value in self.steps:
+            if self.time >= i:
+                val = value
+        return val
+
+
 class LinearSchedule(Schedule):
     def __init__(self, start, end, total_iterations):
         self.start = start
