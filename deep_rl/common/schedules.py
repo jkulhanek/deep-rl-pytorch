14f340e250344fc6151ffc1c12809a6516abac6e
Jonáš Kulhánek <jonas.kulhanek@live.com>
2019-03-26 20:07:41 +0000

    Update multistep schedule

--- deep_rl/common/schedules.py
+++ deep_rl/common/schedules.py
@@ -1,20 +1,5 @@
 from ..core import Schedule
 
-class MultistepSchedule(Schedule):
-    def __init__(self, initial_value, steps, total_iteractions):
-        self.initial_value = initial_value
-        self.steps = steps
-        self.total_iteractions = total_iteractions
-        self.time = None
-
-    def __call__(self):
-        val = self.initial_value
-        for i, value in self.steps:
-            if self.time >= i:
-                val = value
-        return val
-
-
 class LinearSchedule(Schedule):
     def __init__(self, start, end, total_iterations):
         self.start = start
@@ -24,3 +9,42 @@ class LinearSchedule(Schedule):
 
     def __call__(self):
         return self.end + (0 if self.time >= self.total_iterations else float(self.start - self.end) * float(self.total_iterations - self.time) / float(self.total_iterations))
+
+
+class ConstantSchedule(Schedule):
+    def __init__(self, value):
+        self.value = value
+
+    def __call__(self):
+        return self.value
+
+class MultistepSchedule(Schedule):
+    def _wrap_schedule(self, schedule):
+        if isinstance(schedule, Schedule):
+            return schedule
+
+        return ConstantSchedule(schedule)
+
+    def __init__(self, initial_value, steps):
+        self.initial_value = self._wrap_schedule(initial_value)
+        self.steps = [(i, self._wrap_schedule(x)) for i, x in steps]
+        self.time = None
+        self.current_schedule = None
+
+    def __call__(self):
+        return self.current_schedule()
+        
+    def _set_current_schedule(self):
+        val = self.initial_value
+        time = 0
+        for i, value in self.steps:
+            if self.time >= i:
+                val = value
+                time = i
+        self.current_schedule = val
+        return time
+    
+    def step(self, time):
+        super().step(time)
+        start_time = self._set_current_schedule()
+        self.current_schedule.step(self.time - start_time)
